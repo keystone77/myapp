80fa14047a88379ebe71c82ea5ba38e5ba988f52
Sungmin Cho <Sungmin Cho>
2017-02-17 18:55:23 +0900

    'test'

---
+++ test.js
@@ -0,0 +1,13 @@
+function numbering(){
+    var i = 0;
+    while(i < 10){
+        document.write(i);
+        i += 1;
+    }   
+}
+ 
+numbering();
+numbering();
+numbering();
+numbering();
+numbering();
